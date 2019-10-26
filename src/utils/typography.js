139c61a758ec47087e7f53f9ef131dffe277def0
Andrew van Rooyen <andrew@nomanini.com>
2019-10-26 17:52:04 +0200

    initial commit

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,17 +1,13 @@
-import Typography from "typography"
-import Wordpress2016 from "typography-theme-wordpress-2016"
+import Typography from 'typography'
+import theme from 'typography-theme-fairy-gates'
 
-Wordpress2016.overrideThemeStyles = () => {
-  return {
-    "a.gatsby-resp-image-link": {
-      boxShadow: `none`,
-    },
-  }
-}
+theme.overrideThemeStyles = () => ({
+  'header a': {
+    backgroundImage: `none`,
+  },
+})
 
-delete Wordpress2016.googleFonts
-
-const typography = new Typography(Wordpress2016)
+const typography = new Typography(theme)
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
